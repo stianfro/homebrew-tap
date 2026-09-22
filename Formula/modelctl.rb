a877bbe67159ef28a72e56f58a5cb5d4eb7e7da8
Stian Frøystein <stianfroy@gmail.com>
2026-09-22 16:54:37 +0900

    chore: update modelctl to 1.2.1 (#5)

--- Formula/modelctl.rb
+++ Formula/modelctl.rb
@@ -1,28 +1,28 @@
 class Modelctl < Formula
   desc "Manage OpenCode models and API tokens"
   homepage "https://stianfro.github.io/modelctl/"
-  version "1.2.0"
+  version "1.2.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/stianfro/modelctl/releases/download/v1.2.0/modelctl_1.2.0_darwin_arm64.tar.gz"
-      sha256 "1d1c06947cfd7fb24c63714e347ccdd460165ff4c4dd880e32de00e076dbffb6"
+      url "https://github.com/stianfro/modelctl/releases/download/v1.2.1/modelctl_1.2.1_darwin_arm64.tar.gz"
+      sha256 "a1025053c4bf552ee4916bc133b8f6d5f88e60a15457d19c231954fe084750f3"
     end
     on_intel do
-      url "https://github.com/stianfro/modelctl/releases/download/v1.2.0/modelctl_1.2.0_darwin_amd64.tar.gz"
-      sha256 "f129195121af249d4a71595a754356960097ce9dcb9b58678024286dd63bceea"
+      url "https://github.com/stianfro/modelctl/releases/download/v1.2.1/modelctl_1.2.1_darwin_amd64.tar.gz"
+      sha256 "318356c18fede30b977dbc8ce4bb746cdb82085139f3542af34ed4ce61f66da2"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/stianfro/modelctl/releases/download/v1.2.0/modelctl_1.2.0_linux_arm64.tar.gz"
-      sha256 "a3054c3ebe609461d4d772ce1f56622b8160e83e466989fc617bb31db9017a04"
+      url "https://github.com/stianfro/modelctl/releases/download/v1.2.1/modelctl_1.2.1_linux_arm64.tar.gz"
+      sha256 "0322fc1a8c1158235d9c20440f7c2d578fc2a40d856f056ace4ae615887cfd1b"
     end
     on_intel do
-      url "https://github.com/stianfro/modelctl/releases/download/v1.2.0/modelctl_1.2.0_linux_amd64.tar.gz"
-      sha256 "c373d4906f627acd9ad05776923ba90cbf8efa25dec3e6c5d5157faa2504178c"
+      url "https://github.com/stianfro/modelctl/releases/download/v1.2.1/modelctl_1.2.1_linux_amd64.tar.gz"
+      sha256 "83ead9ee605a4e936f964b7eaed043baef13b85bcb6fd973372fe8f755637db7"
     end
   end
 
